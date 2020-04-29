b470d57f2bc308d0597b744dd8e64f16295ad7b1
小澤悠 <ozawashu@MacBook-Pro.local>
2020-04-29 23:38:46 +0900

    introduce_methodを追加

--- sample.rb
+++ sample.rb
@@ -3,13 +3,22 @@ class User
 
  def initialize(**params)
      @name = params[:name]
+     @age = params[:age]
     end
+
+def introduce
+  if @age > 20
+    "はじめまして、私の名前は#{@name}でえす。おちんぽなめたろうは#{@name}のことだッ！！"
+  else
+    "僕は#{@name}だッッ！！ショタコンばばあは失せていただこう"
+  end
+end
 end
 
-def build_users(names)
+def build_users(params)
   users = []
- names.each do |name|
-   users << User.new(name: name)
+ params.each do |params|
+   users << User.new(params)
 end
 
 users
@@ -17,11 +26,12 @@ end
 
 
 
-names = ["saito", "ozawa", "kamizato"]
+params = [{name: "saito", age: 22},  {name: "ozawa", age: 19}, {name: "kamizato", age: 99}]
 
-users = build_users(names)
+users = build_users(params)
 
 
 users.each do |user|
   puts user.name
+  puts user.introduce
 end
